aa4f5a1f0af765d7e71191802c0eb2ea6a7b4b9b
Sergii Gorbachov <sgorbachov@slack-corp.com>
2023-06-15 20:48:53 -0700

    added state

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
 import { type ModalButtonsProps, withModal } from './Modal'
+import { useAppState } from './AppState'
 
 interface SettingsProps extends ModalButtonsProps {
 }
 
 const SettingsModal: React.FC<SettingsProps> = () => {
+  const { appState, setAppState } = useAppState()
+
+  const handleToggleEnv = (): void => {
+    const newEnv = appState.env === 'dev' ? 'prod' : 'dev'
+    setAppState({ ...appState, env: newEnv })
+    console.log('appState:', appState)
+  }
+
   return (
     <div>
       <h1>Settings</h1>
       <p>This is Editor settings page</p>
+      <button onClick={handleToggleEnv}>Toggle Environment</button>
     </div>
   )
 }
